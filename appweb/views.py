c48c4c7de8e173e452be0bb4f5bf49f43770dfc6
MatiasKDU <mvillalobos@kdu.cl>
2025-02-20 11:38:08 -0300

    Vistas modificadas con login (20-02-2025)

--- appweb/views.py
+++ appweb/views.py
@@ -1,18 +1,47 @@
 from django.shortcuts import render
-
-from rest_framework.decorators import action
-
+from rest_framework.decorators import action, api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
-
-from rest_framework import viewsets
-
-from .serializer import UsuarioSerializer, ComputadorSerializer, ActivoSerializer, AccionCrudSerializer, AsignacionSerializer, CaractHardwareSerializer, CaractSoftwareSerializer, DispositivoSerializer, EstadoActivoSerializer, LicenciaSerializer, MantencionActivoSerializer, ProcesoServidorSerializer, TipoServidorSerializer, ServidorSerializer, RegistroaccionSerializer, TipoUsuarioSerializer, SistemaOperativoSerializer
-
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from rest_framework import viewsets, status
+from django.shortcuts import get_object_or_404
+from .serializer import UsuarioSerializer, ComputadorSerializer, ActivoSerializer, AccionCrudSerializer, AsignacionSerializer, CaractHardwareSerializer, CaractSoftwareSerializer, DispositivoSerializer, EstadoActivoSerializer, LicenciaSerializer, MantencionActivoSerializer, ProcesoServidorSerializer, TipoServidorSerializer, ServidorSerializer, RegistroaccionSerializer, TipoUsuarioSerializer, SistemaOperativoSerializer, UserSerializer
 from .models import Usuario, Computador, Activo, AccionCrud, Asignacion, CaractHardware, CaractSoftware, Dispositivo, EstadoActivo, Licencia, MantencionActivo, ProcesoServidor, TipoServidor, Servidor, Registroaccion, TipoUsuario, SistemaOperativo
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
+#---------------------------------Inicio Funcion del Login (20-02-2025)----------------------------------------------------------------------------
+
+@api_view(['POST'])
+def login(request):
+    print(request.data) #datos que yo le envio
+
+    user = get_object_or_404(User, username=request.data['username'])
+
+    if not user.check_password(request.data['password']):
+        return Response({"error":"Contraseña incorrecta!"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+
+    return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
 
-# Create your views here.
 
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def profile(request):
+
+    print(request.user)
+    return Response("Estas logueado como {}".format(request.user.username), status=status.HTTP_200_OK)
+
+
+#---------------------------------Fin Funcion del Login (20-02-2025)----------------------------------------------------------------------------
+
+
+
+#----------------------------------Inicio Funciones del CRUD Clases (20-02-2025)-------------------------------------------------------------------
 class UsuarioView(viewsets.ModelViewSet):
 
     serializer_class = UsuarioSerializer
@@ -889,3 +918,5 @@ class SistemaOperativoView(viewsets.ModelViewSet):
         elif request.method == 'DELETE': 
             sistemaoperativo.delete()
             return Response({"mensaje": "Registro de Accion eliminada correctamente!"}, status=204)
+        
+#-----------------------------------Fin Funciones del CRUD Clases (20-02-2025)----------------------------------------------------------------------
